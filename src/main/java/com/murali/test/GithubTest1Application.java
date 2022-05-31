e270a2f5204d1ada29bfbf49e9dd382d78965508
unknown <murali.kumar@saartha.com>
2022-05-31 11:37:07 +0530

    Test Message

---
+++ src/main/java/com/murali/test/GithubTest1Application.java
@@ -0,0 +1,21 @@
+package com.murali.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubTest1Application {
+
+	@GetMapping("/ping")
+	public String ping() {
+		return "Micro Service Up and Running now.";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTest1Application.class, args);
+	}
+
+}
